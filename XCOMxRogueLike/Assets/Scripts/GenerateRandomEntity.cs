b35119f5a6ae4d5e9d79e23a133301f1a482e754
Tristan Cheah <cheahtristan@hotmail.sg>
2019-09-27 15:22:50 +0800

    Enemy entity generation
    Random entity generation test, able to configure multiple templates to be used in a level generator, which will randomly choose a template to use.

---
+++ XCOMxRogueLike/Assets/Scripts/GenerateRandomEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRandomEntity : MonoBehaviour
+{
+    public GameObject[] game_objects_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int random_number = Random.Range(0, game_objects_.Length);
+        Instantiate(game_objects_[random_number], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

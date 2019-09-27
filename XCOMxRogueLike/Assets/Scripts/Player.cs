40fcf62870d43c337250897329d0dc5e17412128
Ryan Tan <ryantan1337@gmail.com>
2019-09-27 17:49:03 +0800

    Re-wrote Player and Skill scripts, added in Weapon script but did not make any changes.

---
+++ XCOMxRogueLike/Assets/Scripts/Player.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int player_health_;
+    private int player_attack_;
+    private int player_range_;
+    private int player_aoe_;
+    private int player_experience_;
+    private int player_level_;
+    private int player_exp_requisite_;
+    private int player_skills_;
+    private int player_total_skill_level_;
+    private int player_turns_;
+
+    void Update()
+    {
+        CheckPlayerSkills();
+    }
+
+    public void AddSkillLevel(int incrementAmount)
+    {
+        player_total_skill_level_ += incrementAmount;
+    }
+
+    public void CheckPlayerSkills()
+    {
+        if (player_level_ > player_total_skill_level_)
+        {
+            //Allow player to put skill point in
+        }
+    }
+
+    public void PlayerLevelUp()
+    {
+        int player_balance_experience = 0;
+
+        //Increase Player level by 1
+        player_level_ += 1;
+
+        //When killing an enemy, if experience gained exceeds player_exp_requisite, spill over to next level
+        if (player_experience_ > player_exp_requisite_)
+        {
+            player_balance_experience = player_experience_ - player_exp_requisite_;
+        }
+
+        //Increase player_exp_requisite for next level
+        player_exp_requisite_ = player_exp_requisite_ * player_level_;
+
+        //Reset player_experience and spill balance experience into next level
+        player_experience_ = 0 + player_balance_experience;
+    }
+
+    public void CalculateRange(Enemy enemy_position)
+    {
+        if(enemy_position_ < player_range_)
+        {
+            CalculateAOE();
+        }
+        else
+        {
+            //Inform player that enemy is out of range
+        }
+    }
+
+    public void CalculateAOE()
+    {
+        //Check all surrounding entities within attack's AOE
+    }
+
+    public int CalculateDamage()
+    {
+        player_attack_ = weapon_attack_ + skill_damage_;
+
+        return player_attack_;
+    }
+
+    public int DamageDoneToEnemy()
+    {
+        int Damage_Done = 0;
+
+        Damage_Done = CalculateDamage() * weapon_onhit_amount_;
+
+        return Damage_Done;
+    }
+
+    public void PlayerTakeDamage(int DamageTaken)
+    {
+        player_health_ -= DamageTaken;
+    }
+
+    public void CalculateTurnsTaken()
+    {
+        //For every turn taken, increment player_turns_
+    }
+}
---
+++ XCOMxRogueLike/Assets/Scripts/Skill.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+    private int skill_id_;
+    private string skill_name_;
+    private int skill_level_;
+    private bool skill_unlocked_;
+
+    //Function to level up existing skill
+    public void SkillLevelUp(Skill SelectedSkill, Player player)
+    {
+        if (SelectedSkill.skill_unlocked_)
+        {
+            SelectedSkill.skill_level_ += 1;
+            player.AddSkillLevel(1);
+        }
+    }
+
+    //Function to unlock new skill
+    public void UnlockNewSkill(Skill PrereqSkill, Skill NewSkill, Player player)
+    {
+        if(PrereqSkill.skill_unlocked_)
+        {
+            NewSkill.skill_unlocked_ = true;
+            NewSkill.skill_level_ += 1;
+            player.AddSkillLevel(1);
+        }
+    }
+}

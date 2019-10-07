60bb148f792cc821b3be9c97e8065836aa08fa4f
Ryan Tan <ryantan1337@gmail.com>
2019-10-07 17:26:17 +0800

    Added in CheckWithinRange function

--- XCOMxRogueLike/Assets/Scripts/Entity/EntityAttack.cs
+++ XCOMxRogueLike/Assets/Scripts/Entity/EntityAttack.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Entity { }
 [System.Serializable]
 public class EntityAttack
 {
-    [SerializeField] private int damage;
-    [SerializeField] private int range;
-    [SerializeField] private int aoe;
+    [SerializeField] private int damage_;
+    [SerializeField] private int range_;
+    [SerializeField] private int aoe_;
 
     public enum Attack
     {
@@ -29,5 +30,23 @@ public class EntityAttack
     public List<Vector3Int> ExecuteAOE()
     {
         return 0;
+    }
+
+    // Targeted Ranged Attacks
+    public bool CheckWithinRange(Constants.RoomTile[,] tile_list, List<Vector3Int> aoe_coordinates_list, ref List<Entity> entitylist)
+    {
+        bool flag = false;
+        foreach(Vector3Int coordinate in aoe_coordinates_list)
+        {
+            if (tile_list[coordinate.x, coordinate.y].is_occupied_)
+            {
+                //if(tile_list[coordinate.x, coordinate.y].entity_.is_enemy_)
+                //{
+                //      entity_list.Add(tile_list[coordinate.x, coordinate.y].entity_);  
+                //}
+                flag = true;
+            }
+        }
+        return flag;
     }
 }

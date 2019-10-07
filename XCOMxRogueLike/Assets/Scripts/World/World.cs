a421e280bc76b974d23f058e2f61c731b665f062
Tristan Cheah <cheahtristan@hotmail.sg>
2019-10-07 12:18:01 +0800

    Added random generation of entities
    Random generation now compatible to tilemap.

--- XCOMxRogueLike/Assets/Scripts/World/World.cs
+++ XCOMxRogueLike/Assets/Scripts/World/World.cs
@@ -21,11 +21,22 @@ public class World : MonoBehaviour
     void LoadEntities()
     {
         // get creeper prefab
-        GameObject creeper_prefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Entities/Creeper.prefab", typeof(GameObject));
+        // GameObject creeper_prefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Entities/Creeper.prefab", typeof(GameObject));
+        List<GameObject> temp_list = new List<GameObject>();
+        string[] ent_guids = AssetDatabase.FindAssets("ent_", new[] { "Assets/Prefabs/Entities" });
+        foreach (string s in ent_guids)
+        {
+            temp_list.Add((GameObject)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(s), typeof(GameObject)));
+        }
+       
+       
         Transform parent_transform = GameObject.Find("Entities").transform;
         for (int i = 0; i < 10; i++)
         {
-            Instantiate(creeper_prefab, parent_transform).GetComponent<EntityMove>().WorldReadyInit();
+            int random_number = Random.Range(0, temp_list.Count);
+          
+            Instantiate(temp_list[random_number], parent_transform).GetComponent<EntityMove>().WorldReadyInit();
+            // Instantiate(creeper_prefab, parent_transform).GetComponent<EntityMove>().WorldReadyInit();
         }
     }
 

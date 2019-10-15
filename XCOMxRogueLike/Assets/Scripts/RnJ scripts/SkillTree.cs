b48729d14b0c527188b1c9794f218b668266dd4f
WSH <supersaihong69@gmail.com>
2019-10-15 09:01:28 +0800

    pushing to merge

--- XCOMxRogueLike/Assets/Scripts/RnJ scripts/SkillTree.cs
+++ XCOMxRogueLike/Assets/Scripts/RnJ scripts/SkillTree.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SkillTree : MonoBehaviour
 {
-    [SerializeField] private List<SkillObject> all_skills_list_;
+    [SerializeField] private List<SkillObject> all_skills_list_
     // Start is called before the first frame update
     void Start()
     {
